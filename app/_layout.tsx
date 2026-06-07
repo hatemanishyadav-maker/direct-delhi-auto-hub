62183dd21aa276a849898f3f84b6f3958eaf853a
hatemanishyadav-maker <hatemanishyadav@gmail.com>
2026-06-07 15:51:55 +0530

    Remove react-native-keyboard-controller from _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,6 @@ import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import React, { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { KeyboardProvider } from "react-native-keyboard-controller";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 import { ErrorBoundary } from "@/components/ErrorBoundary";
@@ -59,9 +58,7 @@ export default function RootLayout() {
             <CartProvider>
               <WishlistProvider>
                 <GestureHandlerRootView>
-                  <KeyboardProvider>
-                    <RootLayoutNav />
-                  </KeyboardProvider>
+                  <RootLayoutNav />
                 </GestureHandlerRootView>
               </WishlistProvider>
             </CartProvider>
